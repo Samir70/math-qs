e431182b182ca1e6b85b5933db391dadff2bf330
Samir70 <sbetmouni@talktalk.net>
2021-11-09 06:41:43 +0000

    display questions from chosen chapter

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,41 @@
 <script setup>
-import { shortAnswerPaths, totalQs } from 'math-q-factory';
+import { shortAnswerPaths, totalQs, topicsToTest, getMathsQs } from 'math-q-factory';
+import { ShortAnswerQ } from "q-show";
+import { ref } from 'vue';
 import ChapterCard from './components/ChapterCard.vue';
-console.log(shortAnswerPaths)
+// console.log(shortAnswerPaths)
 let chapterSet = new Set(Object.keys(shortAnswerPaths))
 console.log(chapterSet)
+const chosenChapter = ref('');
+const qPathList = ref([]);
+const qNumber = ref(0);
+const currentQ = ref({});
 const setReviewChapter = (title) => {
   console.log('user wants to review', title)
 }
 const setLearnChapter = (title) => {
   console.log('user wants to learn', title)
+  chosenChapter.value = title
+  qPathList.value = topicsToTest.filter(t => t[0] === title)
+  currentQ.value = getMathsQs(...qPathList.value[0])
+  console.log(currentQ.value);
+}
+const respondToAns = (ans) => {
+  console.log('Need to respond to answer:', ans);
+  qNumber.value = (qNumber.value + 1) % qPathList.value.length
+  currentQ.value = getMathsQs(...qPathList.value[qNumber.value])
+}
+const qTypes = {
+  // match: MatchQ,
+  // multiChoice: MultipleChoiceQ,
+  shortAnswer: ShortAnswerQ
 }
 </script>
 
 <template>
   <div>
     <h1>Maths - Qs</h1>
-    <div class="card-list">
+    <div v-if="chosenChapter === ''" class="card-list">
       <ChapterCard
         v-for="chapter in chapterSet"
         v-bind:title="chapter"
@@ -23,6 +43,14 @@ const setLearnChapter = (title) => {
         v-bind:totalQs="totalQs[chapter] || 0"
         v-on:review-chapter="setReviewChapter"
         v-on:learn-chapter="setLearnChapter"
+      />
+    </div>
+    <div v-else>
+      <component
+        v-bind:is="qTypes[currentQ.qType]"
+        v-bind:qData="currentQ"
+        v-bind:key="currentQ.qType + qNumber"
+        v-on:user-answered="respondToAns"
       />
     </div>
   </div>
