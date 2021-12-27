cd6af6fc99e4231c2489e6ca81c9684f7d9e8bee
Samir70 <sbetmouni@talktalk.net>
2021-12-27 21:47:38 +0000

    make fake login status

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,23 @@
+<script setup>
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+const loggedIn = ref(false)
+const loggingIn = ref(false)
+const router = useRouter();
+const onLogin = () => {
+  console.log('user logged in')
+  loggedIn.value = true
+  loggingIn.value = false
+  router.push('/')
+}
+</script>
+
 <template>
   <h1>Maths Qs</h1>
-  <p>You are not logged in. <router-link to="/login">Login</router-link> if you want to save progress</p>
-  <router-view></router-view>
+  <p v-if="!loggedIn && !loggingIn">
+    You are not logged in.
+    <router-link to="/login" v-on:click="loggingIn = true">Login</router-link> if you want to save progress
+  </p>
+  <button v-if="loggedIn" v-on:click="loggedIn = false">Log out</button>
+  <router-view v-on:login="onLogin" ></router-view>
 </template>
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,7 +1,9 @@
 <script setup>
 import { ref } from "vue";
+const emits = defineEmits(['login']);
 </script>
 
 <template>
     <h1>Login Page</h1>
+    <button v-on:click="$emit('login')">Log in</button>
 </template>
