b92d8f06ec815c4a6f7fd6f7b4bce9d8f8ef35f0
Samir70 <sbetmouni@talktalk.net>
2021-12-28 12:45:29 +0000

    extract routes to its own file

--- src/main.js
+++ src/main.js
@@ -1,17 +1,10 @@
 import { createApp } from 'vue';
 import { createRouter, createWebHashHistory } from 'vue-router';
 import { store } from './store';
+import { routes } from './routes';
 import App from './App.vue';
 
-// components for different routes
-import Questions from './components/Questions.vue';
-import Login from './components/Login.vue';
 import '../node_modules/q-show/dist/style.css';
-
-const routes = [
-    { path: '/login', name: 'Login', component: Login },
-    { path: '/', name: 'Questions', component: Questions }
-];
 
 const router = createRouter({
     history: createWebHashHistory(),
---
+++ src/routes.js
@@ -0,0 +1,9 @@
+import ChooseWorkSheet from './components/ChooseWorkSheet.vue'
+import Login from './components/Login.vue';
+import Questions from './components/Questions.vue';
+
+export const routes = [
+    { path: '/login', name: 'Login', component: Login },
+    { path: '/choose_work_sheet', name: 'Worksheet List', component: ChooseWorkSheet },
+    { path: '/', name: 'Questions', component: Questions }
+];
