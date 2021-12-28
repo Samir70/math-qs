fd4a528544b3229ac68258242cda3e2e0a2a65ce
Samir70 <sbetmouni@talktalk.net>
2021-12-28 14:02:56 +0000

    enable changeQs and showAnswers in ws view

--- src/components/ShowWorksheet.vue
+++ src/components/ShowWorksheet.vue
@@ -5,7 +5,7 @@ import { getMathsQs } from 'math-q-factory';
 const workSheetQs = ref(store.state.chosenQs.map(q => getMathsQs(...q)))
 const showAnswers = ref(false);
 const copyWS = () => {
-  navigator.clipboard.writeText(workSheetQs.value.map(q => q.q).join('\n\n'))
+    navigator.clipboard.writeText(workSheetQs.value.map(q => q.q).join('\n\n'))
 }
 </script>
 
@@ -18,8 +18,10 @@ const copyWS = () => {
         </h2>
         <div id="action-buttons">
             <button v-on:click="copyWS">Copy to clipboard</button>
-            <button v-on:click="chooseWorkSheet(chosenWorkSheet.value)">Change all qs</button>
-            <button v-on:click="showAnswers = !showAnswers">Show answers</button>
+            <button
+                v-on:click="workSheetQs = store.state.chosenQs.map(q => getMathsQs(...q))"
+            >Change all qs</button>
+            <button v-on:click="showAnswers = !showAnswers">{{ showAnswers ? 'Hide' : 'Show' }} answers</button>
             <button v-on:click="toShow = 'question'">Show as quiz</button>
         </div>
         <div id="worksheet">
