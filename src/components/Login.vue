313f12035976bcf4a1015eb49dd6ca5b1a46aab0
Samir70 <sbetmouni@talktalk.net>
2021-12-30 17:20:00 +0000

    set up .env for apiKey etc

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -2,6 +2,16 @@
 import { store } from '../store';
 import { useRouter } from 'vue-router';
 const router = useRouter();
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: "maths-qs.firebaseapp.com",
+  projectId: "maths-qs",
+  storageBucket: "maths-qs.appspot.com",
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
+  measurementId: import.meta.env.VITE_measurementId
+};
+// console.log('From Login: ', firebaseConfig)
 const login = () => {
     store.commit('login')
     store.commit('changeUser', 'Guest')
