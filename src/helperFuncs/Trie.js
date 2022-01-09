fea3e380bff4767debcfa1d5e88cab50e11aca92
Samir70 <sbetmouni@talktalk.net>
2022-01-09 22:03:29 +0000

    add Trie class

---
+++ src/helperFuncs/Trie.js
@@ -0,0 +1,37 @@
+class trieNode {
+    constructor(name, q) {
+        this.key = name;
+        this.children = {};
+        this.qList = new Set(q)
+    }
+}
+
+class Trie {
+    constructor() {
+        this.head = {
+            val: null,
+            children: {}
+        }
+    }
+
+    insert(word, qPath) {
+        var current = this.head;
+        for (var w of word) {
+            if (current.children[w] === undefined) {
+                current.children[w] = new trieNode(w, qPath)
+            } else {
+                current.children[w].qList.add(qPath)
+            }
+            current = current.children[w]
+        }
+    }
+
+    search(word) {
+        var current = this.head;
+        for (var w of word) {
+            if (current.children[w] === undefined) { return []}
+            current = current.children[w]
+        }
+        return current.qList;
+    }
+}
