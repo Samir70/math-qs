9efc153739a1b759b71096393d3866c8de737d88
Samir70 <sbetmouni@talktalk.net>
2022-01-11 06:03:47 +0000

    fix bug so all paths get into qTrie

--- src/assets/topicsToTest.js
+++ src/assets/topicsToTest.js
@@ -5,13 +5,11 @@ export const qPaths = topicsToTest.map(t => [...t.path, t.rating]);
 
 export const qTrie = new Trie()
 
-let seenWords = new Set();
 for (let topic of topicsToTest) {
     if (!['shortAnswer', 'multiChoice', 'sort'].includes(topic.qType)) {continue}
     let slug = topic.path.join('-');
     for (let word of topic.path) {
-        if (seenWords.has(word) || word === '') { continue }
-        seenWords.add(word);
+        if (word === '') { continue }
         qTrie.insert(word, slug);
     }
 }
