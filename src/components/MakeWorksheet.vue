229b798b303ad815a05c6cc59bd958330f4dc36f
Samir70 <sbetmouni@talktalk.net>
2022-01-13 06:02:59 +0000

    include current ws when making new ws

--- src/components/MakeWorksheet.vue
+++ src/components/MakeWorksheet.vue
@@ -1,9 +1,11 @@
 <script setup>
 import { ref, watch } from 'vue';
+import { store } from '../store';
 import { qTrie } from '../assets/topicsToTest';
 // console.log(JSON.stringify(qTrie))
 const searchTerm = ref('');
 const foundQs = ref([])
+const currentWS = ref(store.state.chosenWorksheet.topicList)
 watch(searchTerm, (newVal, oldval) => {
     let words = newVal.split(' ')
     // console.log('MWS: searching for', words)
@@ -25,10 +27,25 @@ watch(searchTerm, (newVal, oldval) => {
     <p
         v-if="foundQs.length === 0"
     >{{ searchTerm === '' ? 'Enter a search term' : `No Qs found for '${searchTerm}'` }}</p>
-    <ul>
-        <li v-for="q of foundQs">{{ q }}</li>
-    </ul>
+    <div id="ws-lists">
+        <div class="ws-list">
+            <h3>Click to add to worksheet</h3>
+            <ul>
+                <li v-for="q of foundQs">{{ q }}</li>
+            </ul>
+        </div>
+        <div class="ws-list">
+            <h3>Current worksheet</h3>
+            <ul>
+                <li v-for="i in currentWS.length">{{currentWS[i-1]}}</li>
+            </ul>
+        </div>
+    </div>
 </template>
 
 <style>
+#ws-lists {
+    display: flex;
+    justify-content: space-between;
+}
 </style>
