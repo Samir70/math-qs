95af4044cbe401c002b47d63e2c82f2bd3666f32
Samir70 <sbetmouni@talktalk.net>
2022-01-16 19:26:06 +0000

    update worksheets if BCG IA

--- src/assets/worksheets.js
+++ src/assets/worksheets.js
@@ -1,14 +1,18 @@
 const gcseIA = {
-    name: 'Initial assessment for GCSE at BCG',
-    topicList: [
-        'number-multiplying-ab*c', 'number-multiplying-ab*cd', 'number-multiplying-ab*c.d',
-        'number-dividing-tablesWithRemainder', 'number-busStop-noCarry2', 'number-busStop-noRemainder'
-    ]
+	name: 'Initial assessment for GCSE at BCG',
+	topicList: [
+		"number-multiplying-ab*c-90", "number-multiplying-ab*cd-95", "number-multiplying-ab*c.d-110",
+		"number-dividing-tablesWithRemainder-80", "number-busStop-noCarry2-110", "number-busStop-noRemainder-150",
+		"fraction-simplify-composite-90",
+		"percent-ofAmount-multiplesOf10%-80", "percent-ofAmount-multiplesOf5%-100",
+		"number-multiplying-cubeDigit-65", "number-multiplying-squareEndIn5-70",
+		"ratio-simplify-noUnits2-80", "ratio-simplify-withUnits-150", "ratio-share-givenTotal-200"
+	]
 }
 
 const starter1 = {
-	"name": "2022/01/17 starter",
-	"topicList": [
+	name: "2022/01/17 starter",
+	topicList: [
 		"number-multiplying-ab*c.d-110",
 		"number-busStop-noRemainder-150",
 		"fraction-ofAmount-manySlices-120",
@@ -19,5 +23,5 @@ const starter1 = {
 }
 
 export const worksheets = [
-    gcseIA, starter1
+	gcseIA, starter1
 ]
