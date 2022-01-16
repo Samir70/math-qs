fee34aae0b392f75195639f616a66a6269515614
Samir70 <sbetmouni@talktalk.net>
2022-01-16 19:28:53 +0000

    Add frac of amount to BCG IA WS

--- src/assets/worksheets.js
+++ src/assets/worksheets.js
@@ -3,7 +3,7 @@ const gcseIA = {
 	topicList: [
 		"number-multiplying-ab*c-90", "number-multiplying-ab*cd-95", "number-multiplying-ab*c.d-110",
 		"number-dividing-tablesWithRemainder-80", "number-busStop-noCarry2-110", "number-busStop-noRemainder-150",
-		"fraction-simplify-composite-90",
+		"fraction-simplify-composite-90", "fraction-ofAmount-manySlices-120",
 		"percent-ofAmount-multiplesOf10%-80", "percent-ofAmount-multiplesOf5%-100",
 		"number-multiplying-cubeDigit-65", "number-multiplying-squareEndIn5-70",
 		"ratio-simplify-noUnits2-80", "ratio-simplify-withUnits-150", "ratio-share-givenTotal-200"
