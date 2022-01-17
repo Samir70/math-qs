652dbb1fdabb11dea2cc254c6165a34baefdc8e0
Samir70 <sbetmouni@talktalk.net>
2022-01-17 16:30:16 +0000

    add fraction-add qs to BCG IA worksheet

--- src/assets/worksheets.js
+++ src/assets/worksheets.js
@@ -4,6 +4,7 @@ const gcseIA = {
 		"number-multiplying-ab*c-90", "number-multiplying-ab*cd-95", "number-multiplying-ab*c.d-110",
 		"number-dividing-tablesWithRemainder-80", "number-busStop-noCarry2-110", "number-busStop-noRemainder-150",
 		"fraction-simplify-composite-90", "fraction-ofAmount-manySlices-120",
+		"fraction-add-sameDenom-80", "fraction-add-diffDenomChangeBoth-230", "fraction-add-mixedNumbers-250",
 		"percent-ofAmount-multiplesOf10%-80", "percent-ofAmount-multiplesOf5%-100",
 		"number-multiplying-cubeDigit-65", "number-multiplying-squareEndIn5-70",
 		"ratio-simplify-noUnits2-80", "ratio-simplify-withUnits-150", "ratio-share-givenTotal-200"
