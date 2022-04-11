b4d1897b4bce7b67b82769396f3015bcc778cbe7
Samir70 <sbetmouni@talktalk.net>
2022-04-11 12:58:27 +0100

    extract firebase config details

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,11 @@
 <script setup>
-import { computed } from 'vue';
-import { useRouter } from 'vue-router';
 import firebase from 'firebase/compat/app';
+import { firebaseConfig } from './firebaseConfig';
 import { store } from './store';
 import NavBar from './components/NavBar.vue';
-firebase.initializeApp({
-  apiKey: import.meta.env.VITE_apiKey,
-  authDomain: "maths-qs.firebaseapp.com",
-  projectId: "maths-qs",
-  storageBucket: "maths-qs.appspot.com",
-  messagingSenderId: import.meta.env.VITE_messagingSenderId,
-  appId: import.meta.env.VITE_appId,
-  measurementId: import.meta.env.VITE_measurementId,
-});
+firebase.initializeApp(firebaseConfig);
 firebase.auth().onAuthStateChanged(user => {
-  console.log('from onAuthStateChanged', user) 
+  console.log('from onAuthStateChanged', user)
   if (user) {
     store.commit('login');
     store.commit('changeUser', user.displayName || 'Guest')
---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: import.meta.env.VITE_apiKey,
+    authDomain: "maths-qs.firebaseapp.com",
+    projectId: "maths-qs",
+    storageBucket: "maths-qs.appspot.com",
+    messagingSenderId: import.meta.env.VITE_messagingSenderId,
+    appId: import.meta.env.VITE_appId,
+    measurementId: import.meta.env.VITE_measurementId,
+}
