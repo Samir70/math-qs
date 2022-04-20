15f6ab6c9e352f8788fdef0adcf6f206928929ba
Samir70 <sbetmouni@talktalk.net>
2022-04-20 13:18:44 +0100

    use worksheet to pick 25 qs for bingo

--- src/components/Bingo.vue
+++ src/components/Bingo.vue
@@ -1,16 +1,30 @@
 <script setup>
 import { onMounted, ref } from 'vue';
+import { getMathsQs } from 'math-q-factory';
 import { store } from '../store';
 import { emitActions } from '../helperFuncs/globalConsts';
 const emits = defineEmits(emitActions)
 const bingoQs = ref([]);
 onMounted(() => {
-    bingoQs.value = [1,2,3,4]
+    const ws = store.state.chosenWorksheet.topicList;
+    console.log('making a bingo game!', ws)
+    let qList = [], q = 0
+    while (qList.length < 25) {
+        if (q >= ws.length) {q = 0}
+        let [chapter, section, qName] = ws[q].split('-')
+        qList.push(getMathsQs(chapter, section, qName))
+        q++
+    }
+    bingoQs.value = qList
 })
 
 </script>
 
 <template>
+<h2>Making a bingo game from {{store.state.chosenWorksheet.name}}</h2>
+<div id="bingo-answerbox">
+
+</div>
 <p>{{bingoQs}}</p>
 </template>
 
