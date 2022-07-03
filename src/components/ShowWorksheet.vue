b98b2e1c1b1170b42cae8febdf7ecfd57221b79b
Samir70 <sbetmouni@talktalk.net>
2022-07-03 06:48:56 +0100

    redo MathJax when q deleted from wSheet

--- src/components/ShowWorksheet.vue
+++ src/components/ShowWorksheet.vue
@@ -41,25 +41,26 @@ const changeAllQs = () => {
 const refreshQ = (path, i) => {
     console.log('showWS wants to refresh Q', i, path)
     let newQ = getMathsQs(...path.split('-'))
-    workSheetQs.value[i-1] = displayableQ(newQ)
+    workSheetQs.value[i - 1] = displayableQ(newQ)
 }
 const addBBs = (bbs, i) => {
     let newQs = bbs.map(path => getMathsQs(...path.split('-'))).map(displayableQ)
-    workSheetQs.value.splice(i-1, 0, ...newQs)
+    workSheetQs.value.splice(i - 1, 0, ...newQs)
     console.log('Need to add these building blocks', bbs, 'at Q', i)
     let newName = store.state.chosenWorksheet.name
-    if (newName.indexOf('(edited)') === -1) {newName += ' (edited)'}
+    if (newName.indexOf('(edited)') === -1) { newName += ' (edited)' }
     store.commit('setWorksheet', { name: newName, topicList: workSheetQs.value.map(q => q.qPath) })
 }
 const removeQ = (qPath, i) => {
     console.log(`User wants to remove q ${i}, which has path ${qPath}`)
-    workSheetQs.value.splice(i-1, 1)
+    workSheetQs.value.splice(i - 1, 1)
     let newName = store.state.chosenWorksheet.name
-    if (newName.indexOf('(edited)') === -1) {newName += ' (edited)'}
+    if (newName.indexOf('(edited)') === -1) { newName += ' (edited)' }
     store.commit('setWorksheet', { name: newName, topicList: workSheetQs.value.map(q => q.qPath) })
     if (workSheetQs.value.length === 0) {
         router.push('./make_worksheet')
     }
+    nextTick(() => MathJax.typeset())
 }
 </script>
 
@@ -79,8 +80,7 @@ const removeQ = (qPath, i) => {
         <div id="worksheet">
             <div v-for="i of workSheetQs.length">
                 <WorksheetQ v-bind:question="workSheetQs[i - 1]" v-bind:qnum="i" v-bind:key="i"
-                    v-bind:class="i % 2 ? 'wsq wsq-blue' : 'wsq wsq-green'" 
-                    v-on:add-bbs="addBBs"
+                    v-bind:class="i % 2 ? 'wsq wsq-blue' : 'wsq wsq-green'" v-on:add-bbs="addBBs"
                     v-on:refresh-q="refreshQ" v-on:remove-q="removeQ" />
             </div>
         </div>
