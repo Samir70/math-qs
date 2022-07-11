881821e6987b594a09619c87dbacbfb9bb47774c
Samir70 <sbetmouni@talktalk.net>
2022-07-11 17:18:28 +0100

    define level limits in topcLevels

--- src/assets/topicLevels.js
+++ src/assets/topicLevels.js
@@ -8,3 +8,15 @@ export const topicLevels = [
     [100, 'FSL1', 'yellow'],
     [-1, 'FSE3', 'white']
 ]
+
+export const levelLimits = {
+    all: {name: 'all', limit: Infinity},
+    fse3: {name: 'Functional skills E3', limit: 100},
+    fsl1: {name: 'Functional skills L1', limit: 250},
+    gcse3: {name: 'GCSE grade 3', limit: 350},
+    gcse45: {name: 'GCSE grade 4 to 5', limit: 500},
+    gcseH: {name: 'GCSE - higher tier', limit: 1000},
+    aLevel: {name: 'A-Level', limit: 1500},
+    degree: {name: 'Degree', limit: 5000},
+    master: {name: 'Masters', limit: Infinity}
+}
