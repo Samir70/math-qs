697fe4403f4d2f52bd49c324215e957eebd5f666
Samir70 <sbetmouni@talktalk.net>
2022-07-11 15:32:00 +0100

    Sort chapter cards by level

--- src/components/ChooseChapter.vue
+++ src/components/ChooseChapter.vue
@@ -5,15 +5,19 @@ import { totalQs } from 'math-q-factory';
 import { qPaths } from '../assets/topicsToTest';
 import ChapterCard from './ChapterCard.vue';
 import { emitActions } from '../helperFuncs/globalConsts';
+import { topicLevels } from "../assets/topicLevels";
 const emits = defineEmits(emitActions)
-let chapterSet = new Set(Object.keys(totalQs))
-console.log('ChooseChapter:', chapterSet);
+const findLevel = n => topicLevels.find(t => t[0] < n)[0]
+let chapterSet = Object.keys(totalQs).sort((a, b) =>
+    findLevel(totalQs[a].min) === findLevel(totalQs[b].min) ?
+        totalQs[a].max - totalQs[b].max : totalQs[a].min - totalQs[b].min)
+// console.log('ChooseChapter:', chapterSet);
 const router = useRouter();
 
 const setLearnChapter = (title) => {
     console.log('user wants to learn', title);
     store.commit('setChapter', title);
-    store.commit('setWorksheet', {name:title, topicList: qPaths.filter(t => t[0] === title).map(t => t.join('-'))});
+    store.commit('setWorksheet', { name: title, topicList: qPaths.filter(t => t[0] === title).map(t => t.join('-')) });
     router.push('/show_worksheet')
 }
 </script>
@@ -22,15 +26,10 @@ const setLearnChapter = (title) => {
     <h1>Choose a Chapter</h1>
     <div class="chapter-card-list">
         <!-- Chapter card no longer making use of contents attribute, which used to list sections -->
-        <ChapterCard
-            v-for="chapter in chapterSet"
-            v-bind:title="chapter"
+        <ChapterCard v-for="chapter of chapterSet" v-bind:title="chapter"
             v-bind:completedQs="store.state.userProgress[chapter] === undefined ? 0 : store.state.userProgress[chapter].size"
-            v-bind:totalQs="totalQs[chapter].qCount || 0"
-            v-bind:minLevel="totalQs[chapter].min"
-            v-bind:maxLevel="totalQs[chapter].max"
-            v-on:click="setLearnChapter(chapter)"
-        />
+            v-bind:totalQs="totalQs[chapter].qCount || 0" v-bind:minLevel="totalQs[chapter].min"
+            v-bind:maxLevel="totalQs[chapter].max" v-on:click="setLearnChapter(chapter)" />
     </div>
 </template>
 
