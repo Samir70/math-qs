dc4436e7e1d760de9db4fa8016993b33763f9fe2
Samir70 <sbetmouni@talktalk.net>
2022-07-14 16:00:15 +0100

    create a ChapterTracker class

--- src/assets/ProgressTracker.js
+++ src/assets/ProgressTracker.js
@@ -1,11 +1,11 @@
 
 // track user progress through a single section of a chapter
 class SectionTracker {
-    constructor(parentChapter = '', sectionName = '', numberOfQsAnswered = 0, numberOfCorrectAnswers = 0, pathsOfAnsweredQs = [], highestRatingAnsweredCorrectly = 0) {
+    constructor(parentChapter = '', sectionName = '', numberOfCorrectAnswers = 0, numberOfQsAnswered = 0, highestRatingAnsweredCorrectly = 0, pathsOfAnsweredQs = []) {
         this.parentChapter = parentChapter;
         this.sectionName = sectionName;
-        this.numberOfQsAnswered = numberOfQsAnswered;
         this.numberOfCorrectAnswers = numberOfCorrectAnswers;
+        this.numberOfQsAnswered = numberOfQsAnswered;
         this.highestRatingAnsweredCorrectly = highestRatingAnsweredCorrectly;
         this.pathsOfAnsweredQs = new Set(pathsOfAnsweredQs)
     }
@@ -24,29 +24,86 @@ class SectionTracker {
 
     // to make a copy of a SectionTracker
     static from(obj) {
+        if (!(obj instanceof SectionTracker)) {
+            return console.error('ERROR:: Cannot make a SectionTracker from ', obj)
+        }
         return new SectionTracker(
-            obj.parentChapter, obj.sectionName, obj.numberOfQsAnswered,
-            obj.numberOfCorrectAnswers,
-            [...obj.pathsOfAnsweredQs],
-            obj.highestRatingAnsweredCorrectly
+            obj.parentChapter, obj.sectionName,
+            obj.numberOfCorrectAnswers, obj.numberOfQsAnswered,
+            obj.highestRatingAnsweredCorrectly,
+            [...obj.pathsOfAnsweredQs]
         )
     }
 }
 
 // track user progress through a single chapter
-class ChapterProgress {
+class ChapterTracker {
+    constructor(chapterName = '', numberOfCorrectAnswers = 0, numberOfQsAnswered = 0, highestRatingAnsweredCorrectly = 0, listOfSections = {}) {
+        this.chapterName = chapterName;
+        this.numberOfCorrectAnswers = numberOfCorrectAnswers;
+        this.numberOfQsAnswered = numberOfQsAnswered;
+        this.highestRatingAnsweredCorrectly = highestRatingAnsweredCorrectly;
+        this.listOfSections = listOfSections
+    }
+    trackNewQ(path = '', correct = false) {
+        let [chapter, section, qName, rating] = path.split('-');
+        if (chapter !== this.chapterName) {
+            return console.error(`ERROR: attempt to update ${this.parentChapter + '-' + this.sectionName} tracker with q: ${path}`);
+        }
+        console.log(`${this.chapterName} is tracking ${path}`)
+        this.numberOfQsAnswered++
+        if (correct) {
+            this.numberOfCorrectAnswers++
+            this.highestRatingAnsweredCorrectly = Math.max(this.highestRatingAnsweredCorrectly, rating)
+        }
+        if (this.listOfSections[section] === undefined) {
+            this.listOfSections[section] = new SectionTracker(chapter, section)
+        }
+        this.listOfSections[section].trackNewQ(path, correct)
+    }
 
+    // to make a copy of a ChapterTracker
+    static from(obj) {
+        if (!(obj instanceof ChapterTracker)) {
+            return console.error('ERROR:: Cannot make a ChapterTracker from ', obj)
+        }
+        let newSectionList = {}
+        for (let section in obj.listOfSections) {
+            newSectionList[section] = SectionTracker.from(obj.listOfSections[section])
+        }
+        return new ChapterTracker(
+            obj.chapterName,
+            obj.numberOfCorrectAnswers, obj.numberOfQsAnswered,
+            obj.highestRatingAnsweredCorrectly, newSectionList
+        )
+    }
 }
 // this class is intended so that it will store all user progress
 class ProgressTracker {
 
 }
 
-const dummySection = new SectionTracker('examQs', 'bakeryQ', 1, 1, ['examQs-bakeryQ-meanNoBags-210'], 210)
-console.log(dummySection)
-dummySection.trackNewQ('examQs-bakeryQ-someOther-250', true)
-console.log(dummySection)
-dummy2 = SectionTracker.from(dummySection)
-dummy2.trackNewQ('examQs-bakeryQ-someOther-260', true)
-dummySection.trackNewQ('examQs-bakeryQ-someOther2-250', true)
-console.log(dummySection, dummy2)
+// to be moved to another file eventually!!
+let qList = [
+    "number-multiplying-ab*c-90", "number-multiplying-ab*cd-95", "number-multiplying-ab*c.d-110",
+    "number-dividing-tablesWithRemainder-80", "number-busStop-noCarry2-110", "number-busStop-noRemainder-150",
+    "fraction-simplify-composite-90", "fraction-ofAmount-manySlices-120",
+    "fraction-add-sameDenom-80", "fraction-add-diffDenomChangeBoth-230", "fraction-add-mixedNumbers-250",
+    "percent-ofAmount-multiplesOf10%-80", "percent-ofAmount-multiplesOf5%-100",
+    "number-multiplying-cubeDigit-65", "number-multiplying-squareEndIn5-70",
+    "ratio-simplify-noUnits2-80", "ratio-simplify-withUnits-150", "ratio-share-givenTotal-200"
+]
+
+let chapters = {}
+for (let q of qList) {
+    let chapter = q.split('-')[0]
+    if (chapters[chapter] === undefined) {
+        chapters[chapter] = new ChapterTracker(chapter)
+    }
+    chapters[chapter].trackNewQ(q, true)
+}
+let numCT = ChapterTracker.from(chapters['number'])
+numCT.trackNewQ('number-something-forTest-500', true)
+numCT.trackNewQ('number-dividing-forTest-300', true)
+console.log(JSON.stringify(chapters, null, 2))
+console.log(JSON.stringify(numCT, null, 2))
