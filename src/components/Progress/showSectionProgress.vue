2fb57f4f7cd856a885ebd420d1554bb064e3c93a
Samir70 <sbetmouni@talktalk.net>
2022-07-17 19:43:21 +0100

    show section progress with green red bar

---
+++ src/components/Progress/showSectionProgress.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { SectionTracker } from "../../assets/ProgressTracker";
+const props = defineProps({
+    section: SectionTracker
+})
+const percentCorrect = Math.round(props.section.numberOfCorrectAnswers / props.section.numberOfQsAnswered * 100)
+const progBar = `linear-gradient(to right, green 0%, green ${percentCorrect}%, red ${percentCorrect}%, red 100%)`
+</script>
+
+<template>
+    <div class="section-bar">
+        <p class="section-name">{{ section.sectionName }}</p>
+        <span class="prog-bar" v-bind:style="{ backgroundImage: progBar }">{{ percentCorrect }}%</span>
+        <p class="section-rating">{{ section.highestRatingAnsweredCorrectly }}</p>
+    </div>
+</template>
+
+<style>
+.section-bar {
+    display: flex;
+    justify-content: space-evenly;
+    align-items: baseline;
+    margin: 1px auto;
+}
+
+.section-name {
+    width: 25%;
+}
+
+.prog-bar {
+    display: block;
+    text-align: center;
+    color: white;
+    width: 50%;
+    height: 1em;
+    border: 1px solid red;
+}
+
+.section-rating {
+    width: 20%;
+}
+</style>
