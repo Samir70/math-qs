d0d8c1aed5284e60595c44297f71f7c73213b866
Samir70 <sbetmouni@talktalk.net>
2022-08-12 10:10:27 +0100

    rename ShowQuestion as ShowQuiz

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ import ChooseWorksheet from './components/ChooseWorksheet.vue';
 import MakeWorksheet from './components/MakeWorksheet.vue';
 import ChooseChapter from "./components/ChooseChapter.vue";
 import ShowWorksheet from './components/ShowWorksheet.vue';
-import ShowQuestion from "./components/ShowQuestion.vue";
+import ShowQuiz from "./components/ShowQuiz.vue";
 import ShowDiagnostic from "./components/ShowDiagnostic.vue";
 import Login from './components/Login.vue';
 import Dashboard from './components/Dashboard.vue';
@@ -14,7 +14,7 @@ export const routes = [
     { path: '/choose_chapter', name: 'ChooseChapter', component: ChooseChapter },
     { path: '/make_worksheet', name: 'MakeWorksheet', component: MakeWorksheet },
     { path: '/show_worksheet', name: 'ShowWorksheet', component: ShowWorksheet },
-    { path: '/show_question', name: 'ShowQuestion', component: ShowQuestion },
+    { path: '/show_question', name: 'ShowQuiz', component: ShowQuiz },
     { path: '/diagnostic', name: 'ShowDiagnostic', component: ShowDiagnostic },
     { path: '/bingo', name: 'BingoQs', component: Bingo },
     { path: '/', name: 'Dashboard', component: Dashboard }
